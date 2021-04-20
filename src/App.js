ebe203c559c9c35cb5d4eed4cb57acc3baa40840
Bilal El Houdaigui <bilal@ebi.ac.uk>
2021-04-20 12:15:52 +0100

    Reverting to the project initial state (commit 07c69a7)

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-/**
- * See the NOTICE file distributed with this work for additional information
- * regarding copyright ownership.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import logo from './logo.svg';
+import './App.css';
 
-import React from 'react';
-import Urls from './Urls';
-import Layout from './components/Layout';
-import {connect} from 'react-redux';
-import * as actions from './store/authActions';
-
-
-function App(props) {
-
-  // Similar to componentDidMount and componentDidUpdate:
-  React.useEffect(() => {
-    props.setAuthenticatedIfRequired();
-  }, []);
-
+function App() {
   return (
     <div className="App">
-      <Layout {...props}>
-         <Urls {...props}/>
-      </Layout>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-//This means that one or more of the redux states in the store are available as props
-const mapStateToProps = (state) => {
-  return {
-    isAuthenticated: state.auth.token !== null && typeof state.auth.token !== 'undefined',
-    token: state.auth.token
-  }
-}
-
-//This means that one or more of the redux actions in the form of dispatch(action) combinations are available as props
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setAuthenticatedIfRequired: () => dispatch(actions.authCheckState()),
-    logout: () => dispatch(actions.authLogout())
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,51 +1,15 @@
-/**
- * See the NOTICE file distributed with this work for additional information
- * regarding copyright ownership.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import authReducer from './store/authReducer';
 import reportWebVitals from './reportWebVitals';
-import {Container} from "@material-ui/core";
-
-
-const reducer = combineReducers({ auth: authReducer }); // Using Combine Reducers here although only one reducer is present.
-// Official explanation here: https://react-redux.js.org/using-react-redux/connect-mapstate#mapstatetoprops-will-not-run-if-the-store-state-is-the-same
-const composeEnhanced = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose // The first one is to make the chrome dev extension work
-const store = createStore(reducer, composeEnhanced(applyMiddleware(thunk))); // We are using thunk, because it allows delaying the dispatch actions
-// Thunk wraps the dispatch actions into custom functions which are available with the mapDispatchToProps
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-        <Container maxWidth="xl">
-            <App />
-        </Container>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
