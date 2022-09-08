69f9bada0f4f3ea46acc4360d1d0e91f67c7cb6b
Bilal <bilal@ebi.ac.uk>
2022-09-08 15:36:40 +0100

    :wrench: setup a proper environment variables for staging, prod and dev

--- src/settings.js
+++ src/settings.js
@@ -14,20 +14,7 @@
  * limitations under the License.
  */
 
-let API_SERVER_VAL = '';
-
-switch (process.env.REACT_APP_ENV) {
-    case 'development':
-        API_SERVER_VAL = 'http://localhost:8000';
-        break;
-    case 'production':
-        API_SERVER_VAL = process.env.REACT_APP_API_SERVER;
-        break;
-    default:
-        API_SERVER_VAL = 'http://localhost:8000';
-        break;
-}
-
-export const API_SERVER = API_SERVER_VAL;
+// REACT_APP_API_SERVER environment variable is fetched from .env.{env}
+export const API_SERVER = process.env.REACT_APP_API_SERVER;
 
 export const SESSION_DURATION = 5*3600*1000;
